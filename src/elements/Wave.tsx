0d366434b974e9fb49a2efa117ed035e02ca0d3d
Luis Kunz <devpluis@gmail.com>
2021-06-08 09:09:10 +0200

    improvements

--- src/elements/Wave.tsx
+++ src/elements/Wave.tsx
@@ -12,21 +12,30 @@ const waveFlow = keyframes`
   }
 `
 
-const Wave: React.FC = () => {
+const Wave: React.FC<{ flip?: boolean }> = ({ flip }) => {
   const prefersReducedMotion = usePrefersReducedMotion()
   const animation = prefersReducedMotion ? undefined : `${waveFlow} 45s cubic-bezier(0.36, 0.45, 0.63, 0.53) infinite`
   return (
-    <Box w="100%" position="absolute" bottom="0" left="0">
+    <Box
+      w="100%"
+      position="absolute"
+      bottom={flip ? `none` : `0`}
+      top={flip ? `0` : `none`}
+      left="0"
+      height="96px"
+      zIndex="10"
+    >
       <Box
-        background={`url(${WaveSvg}) repeat-x`}
+        background={`url(${WaveSvg.default}) repeat-x`}
         position="absolute"
-        top="-96px"
+        bottom="0"
         width="6400px"
         height="96px"
         animation={animation}
+        transform={flip ? `rotate(180deg) translate3d(0, 0, 0)` : `translate3d(0, 0, 0)`}
       />
     </Box>
   )
 }
 
-export default Wave
+export { Wave }
