f2f100479f108361c66284e14036ba9708313ddc
Luis Kunz <devpluis@gmail.com>
2021-06-08 09:07:39 +0200

    update global components

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,17 @@
 import React from "react"
 import { Header } from "./blocks/header"
 import { CookieBanner } from "./CookieBanner/CookieBanner"
+import { Footer } from "../components/footer"
 
-export const Layout = ({ children }) => (
+interface ILayoutProps {
+  navBlack?: boolean
+  children: React.ReactChild
+}
+export const Layout = ({ children, navBlack = false}: ILayoutProps) => (
   <>
     <CookieBanner content="Hallo" buttonText="Accept" />
-    <Header />
+    <Header navBlack={navBlack} />
     {children}
+    <Footer />
   </>
 )
