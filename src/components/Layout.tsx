b97af57586330b20783b257d56a4524058d6c524
Luis Kunz <devpluis@gmail.com>
2021-06-13 16:37:07 +0200

    add self-hostable font: Montserrat

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,7 +3,11 @@ import { Header } from "./blocks/header"
 import { CookieBanner } from "./CookieBanner/CookieBanner"
 import { Footer } from "../components/footer"
 import "../prism.css"
+import "@fontsource/montserrat/400.css"
+import "@fontsource/montserrat/500.css"
+import "@fontsource/montserrat/700.css"
 
+// Defaults to weight 400.
 interface ILayoutProps {
   navBlack?: boolean
   children?: React.ReactNode
