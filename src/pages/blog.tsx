7e4187f608e66ef7062cf9bb1bd291a519856962
Luis Kunz <devpluis@gmail.com>
2021-06-14 16:39:16 +0200

    finish todo

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -16,35 +16,31 @@ const Blog = ({
   data: {
     BlogPost: { nodes: blogPost },
   },
-}: IBlog) => {
-  console.log(blogPost)
-  if (!blogPost) return null
-  return (
-    <Layout>
-      {/* TODO: Seo content */}
-      <SEO title="Luis Kunz | Blog" description="TODO" />
-      <Hero
-        headline="Blog"
-        subheading="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac elementum scelerisque dui mattis nulla."
-      />
-      <FullWidthContainer variant="max" pt={space.section}>
-        <Grid templateColumns={[`100%`, `repeat(3, minmax(250px, 1fr))`]}>
-          {blogPost.map((post) => (
-            <PostCard
-              key={post.id}
-              title={post.data.title}
-              tags={post.tags}
-              image={post.data.blogImage}
-              date={post.data.postDate}
-              desc={post.data.description}
-              location={post.url}
-            />
-          ))}
-        </Grid>
-      </FullWidthContainer>
-    </Layout>
-  )
-}
+}: IBlog) => (
+  <Layout>
+    {/* TODO: Seo content */}
+    <SEO title="Luis Kunz | Blog" description="TODO" />
+    <Hero
+      headline="Blog"
+      subheading="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac elementum scelerisque dui mattis nulla."
+    />
+    <FullWidthContainer variant="max" pt={space.section}>
+      <Grid templateColumns={[`100%`, `repeat(3, minmax(250px, 1fr))`]}>
+        {blogPost.map((post) => (
+          <PostCard
+            key={post.id}
+            title={post.data.title}
+            tags={post.tags}
+            image={post.data.blogImage}
+            date={post.data.postDate}
+            desc={post.data.description}
+            location={post.url}
+          />
+        ))}
+      </Grid>
+    </FullWidthContainer>
+  </Layout>
+)
 
 export default Blog
 
